8693224c7ffbc389897625d48726df5ac5ec5644
Gaston Martres <gastonmartres@gmail.com>
2021-12-17 18:14:35 -0300

    stress

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from filelock import FileLock
-import time
+import time,os,random
+
+
 
 while True:
+    for x in range(0,9999): 
+        f = open("/tmp/myfile.txt","+a")
+        string = str(random.randrange(9999,99999999))
+        print(string)
+        f.write(string)
+        f.close
     with FileLock("/tmp/myfile.txt"):
         # work with the file as it is now locked
         print("Lock acquired.")
-        time.sleep(10)
+        os.remove("/tmp/myfile.txt")
+        time.sleep(.1)
 

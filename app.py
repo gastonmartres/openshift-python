041f0f67546e988260b0d6368055cd1c3b00c282
Gaston Martres <gastonmartres@gmail.com>
2021-12-17 17:47:52 -0300

    Sleep

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from filelock import FileLock
 import time
 
 while True:
-    with FileLock("myfile.txt"):
+    with FileLock("/tmp/myfile.txt"):
         # work with the file as it is now locked
         print("Lock acquired.")
         time.sleep(10)
